ccce997c88169b804499129714e6222498608518
Sachin Vidhate <sachin.vidhate@venturesunited.in>
2018-11-11 13:15:23 +0530

    added test code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { AppRoutingModule } from './app.routing';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   imports: [
@@ -52,7 +53,8 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
     PerfectScrollbarModule,
     BsDropdownModule.forRoot(),
     TabsModule.forRoot(),
-    ChartsModule
+    ChartsModule,
+    ModalModule.forRoot()
   ],
   declarations: [
     AppComponent,
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare var jQuery:any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+
+export class LoginComponent { 
+  public infoModal;
+
+  constructor(public router: Router){   
+  }
+  showLoginModal(){
+    jQuery('#openLoginModal').trigger('click');
+  }
+
+  login(){
+    jQuery('#closeLoginModal').trigger('click');
+    this.router.navigateByUrl('dashboard');
+  }
+}
