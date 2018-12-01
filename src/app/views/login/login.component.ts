43e9ff535cf3223be565ec94d0d809c20ad1e7ce
Sachin Vidhate <vidhatesachin@gmail.com>
2018-12-02 00:30:58 +0530

    email verification

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -16,16 +16,63 @@ export class LoginComponent{
   public infoModal;
   public loginModel:Login = new Login();
 
+  public showValiduser=false;
+  public showValidPassword=false;
+  public showValidEmail=false;
+
   constructor(public router: Router, public authService:AuthenticationService){ 
 
   }
   showLoginModal(){
     jQuery('#openLoginModal').trigger('click');
   }
+  validateUser(){
+    if(this.loginModel.username==null||this.loginModel.username==""){
+      this.showValiduser=true;
+      this.showValidEmail=false
+      }
+      else
+      {
+        this.showValiduser=false;
+      }
+  }
+  validatePassword(){
+    if(this.loginModel.password==null|| this.loginModel.password==""){
+      this.showValidPassword=true;
+    }
+    else
+    {
+      this.showValidPassword=false;
+    } 
+    //console.log(this.loginModel.username);{}
+  }
+  validateEmail(){
+  this.validateUser();
+    if(this.loginModel.username!=null && this.loginModel.username!="")
+    {
+    
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let testResult:boolean= re.test(String(this.loginModel.username).toLowerCase());
+    /* alert (testResult);
+  */   if(testResult){
+      this.showValidEmail=false;
+
+    }
+    else
+    {
+      this.showValidEmail=true;
+    }
+
+  }
+  
+  }
+
+   
   login(){
-    //jQuery('.closeLoginModalClass').trigger('click');
+    jQuery('.closeLoginModalClass').trigger('click');
+    this.router.navigateByUrl('home');
     console.log(this.loginModel.username + " and "+ this.loginModel.password);
-    this.authService.authenticateUser(this.loginModel).subscribe(
+    /* this.authService.authenticateUser(this.loginModel).subscribe(
       (response:any) => {
         console.info("Response"+response);
         jQuery('#closeLoginModal').trigger('click');
@@ -37,7 +84,7 @@ export class LoginComponent{
         console.log(error);
       }
     ) 
-
+ */
 
     
   }
